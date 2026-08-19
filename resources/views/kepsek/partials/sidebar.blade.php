08288a789aa5be47ae4a75cd0a0172e608c6734b
PradiptaPPLG <pradipta02032009@gmail.com>
2026-08-19 07:14:23 +0700

    feat: create sidebar partial for Kepala Sekolah dashboard

--- resources/views/kepsek/partials/sidebar.blade.php
+++ resources/views/kepsek/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     {{-- Sidebar Header / Brand --}}
     <div class="sidebar-brand">
         <div class="sidebar-brand-icon" style="background: none;">
-            <img src="/icons/logoadmin.png" alt="Logo" style="width: 32px; height: 32px; object-fit: contain;">
+            <img src="/icons/logoadmin.png" alt="Logo" style="width: 42px; height: 42px; object-fit: contain;">
         </div>
         <div class="sidebar-brand-text">
             <div class="sidebar-brand-name">Asign</div>
